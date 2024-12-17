2371f382f2389c218eb371789f36233d7c535d15
Matheqs <matheqs@outlook.com>
2024-12-17 03:36:25 -0300

    Detail page + header partials

--- index.js
+++ index.js
@@ -10,14 +10,23 @@ const PORT = 3000; //declaração da porta a ser usada. Uso 3000 por padrão.
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static("public")); //não é exatamente sobre middleware, mas é importante para declaração da pasta public como referencia de assets
-//função teste
+//função teste homepage
 app.get("/", (req, res) => {
     try{
        return res.render("index.ejs");
     }catch(err){
         console.error(err.message);
     };
-  });
+});
+//função teste detalhes
+app.get("/detail", (req, res) => {
+    try{
+        return res.render("detail.ejs");
+     }catch(err){
+         console.error(err.message);
+    };
+});
+
 //função escutadora
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
